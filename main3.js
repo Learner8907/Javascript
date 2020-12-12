42544fbbe80518c1133c64f693caee2bc05fd113
Learner8907 <72011994+Learner8907@users.noreply.github.com>
2020-12-12 13:05:04 +0530

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,31 @@
+var fn="Sharanya";
+console.log(fn);
+
+var ln="Tiwari";
+console.log(ln);
+
+var wn=fn.concat(ln);
+console.log(wn);
+
+var a=98;
+var b=34;
+var sum=a+b;
+console.log(sum);
+
+var c=56
+var d=2
+var div=c/d;
+console.log(div)
+
+var e=146
+var f=789
+var pro=e*f;
+console.log(pro);
+
+var g=67
+var h=34
+var sub= g-h;
+console.log(sub);
+
+
+
